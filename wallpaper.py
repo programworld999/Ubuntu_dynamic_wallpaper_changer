2899260197ff9f9e4d45d65e9b058aafe1980d59
programworld999 <programworld999@gmail.com>
2019-04-20 19:17:11 +0530

    local image set added

--- wallpaper.py
+++ wallpaper.py
@@ -1,16 +1,17 @@
 import os
+import validators
 import urllib.request as DFU
 
 
 def downloadFile(url):
 	print('Processing.........')
-	DFU.urlretrieve(url, '.wallpaper.jpg')
-	setwallpaper()
+	DFU.urlretrieve(url, '/home/programworld999/Desktop/python/Python/wallpaper/.wallpaper.jpg')
+	setwallpaper('.wallpaper.jpg')
 
 
-def setwallpaper():
+def setwallpaper(image):
 	try:
-		os.system('cp .wallpaper.jpg /home/programworld999/Pictures/Wallpapers')
+		os.system('cp /home/programworld999/Desktop/python/Python/wallpaper/.wallpaper.jpg /home/programworld999/Pictures/Wallpapers')
 		os.system('clear')
 		print('\033[1m \033[92m Wallpaper Change Successful! \033[0m')
 	except:
@@ -18,4 +19,11 @@ def setwallpaper():
 
 image = input('Enter Image URL: ')
 
-downloadFile(image)
+url_cheak = validators.url(image)
+if url_cheak == True:
+	print('Online Image')
+	downloadFile(image)
+else:
+	print('Local Image')
+	os.system('cp '+image+' /home/programworld999/Desktop/python/Python/wallpaper/.wallpaper.jpg')
+	setwallpaper('null')
